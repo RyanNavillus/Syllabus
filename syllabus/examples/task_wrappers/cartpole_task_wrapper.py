36e6ca93144bb782d03728b67945793fa05bde86
Ryan Sullivan <ryanpnavillus@gmail.com>
2023-03-27 03:56:59 -0400

    Move task wrappers to separate folder

---
+++ syllabus/examples/task_wrappers/cartpole_task_wrapper.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+from syllabus.core import TaskWrapper
+
+
+class CartPoleTaskWrapper(TaskWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.task = (-0.02, 0.02)
+        self.total_reward = 0
+
+    def reset(self, *args, **kwargs):
+        self.env.reset()
+        self.total_reward = 0
+        if "new_task" in kwargs:
+            new_task = kwargs.pop("new_task")
+            self.change_task(new_task)
+        return np.array(self.env.state, dtype=np.float32)
+
+    def change_task(self, new_task):
+        low, high = new_task
+        self.env.state = self.env.np_random.uniform(low=low, high=high, size=(4,))
+        self.task = new_task
+
+    def _task_completion(self, obs, rew, done, info) -> float:
+        # Return percent of optimal reward
+        self.total_reward += rew
+        return self.total_reward / 500.0
