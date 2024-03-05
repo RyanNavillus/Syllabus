6015b325a33df9059a6566deb671e2854c51f8ef
Ryan Sullivan <ryanpnavillus@gmail.com>
2024-03-05 01:53:32 -0500

    Fix bad merge

--- syllabus/core/__init__.py
+++ syllabus/core/__init__.py
@@ -6,6 +6,7 @@ from .utils import decorate_all_functions, UsageError, enumerate_axes
 from .curriculum_base import Curriculum
 from .curriculum_sync_wrapper import (CurriculumWrapper,
                                       MultiProcessingCurriculumWrapper,
+                                      MultiProcessingComponents,
                                       RayCurriculumWrapper,
                                       make_multiprocessing_curriculum,
                                       make_ray_curriculum)
