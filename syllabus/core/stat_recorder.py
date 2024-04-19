a6de4f97dfa3bcc98bcb02093e5d38c7f3be9365
allisony9 <xcyang@umd.edu>
2024-04-19 17:31:03 -0400

    remove keep_past_n

--- syllabus/core/stat_recorder.py
+++ syllabus/core/stat_recorder.py
@@ -12,23 +12,25 @@ class StatRecorder:
     Individual stat tracking for each task.
     """
 
-    def __init__(self, task_space: TaskSpace, keep_last_N=10, calc_past_N=None):
+    def __init__(self, task_space: TaskSpace, calc_past_n=None):
         """Initialize the StatRecorder"""
 
         self.task_space = task_space
-        self.calc_past_N = calc_past_N
+        self.calc_past_n = calc_past_n
 
         assert isinstance(self.task_space, TaskSpace), f"task_space must be a TaskSpace object. Got {type(task_space)} instead."
         assert isinstance(self.task_space.gym_space, Discrete), f"Only Discrete task spaces are supported. Got {type(task_space.gym_space)}"
-        if calc_past_N and calc_past_N > keep_last_N:
-            warnings.warn("The number of data points requested to calculate statistics exceeds the number of data points kept in memory. Will only use data points available in memory to calculate statistics instead.")
 
         self.tasks = self.task_space.get_tasks()
         self.num_tasks = self.task_space.num_tasks
 
-        self.episode_returns = {task: deque(maxlen=keep_last_N) for task in self.tasks}
-        self.episode_lengths = {task: deque(maxlen=keep_last_N) for task in self.tasks}
-        self.env_ids = {task: deque(maxlen=keep_last_N) for task in self.tasks}
+        if self.calc_past_n is not None:
+            self.episode_returns = {task: deque(maxlen=calc_past_n) for task in self.tasks}
+            self.episode_lengths = {task: deque(maxlen=calc_past_n) for task in self.tasks}
+            self.env_ids = {task: deque(maxlen=calc_past_n) for task in self.tasks}
+        else:
+            self.num_past_episodes = {task: 0 for task in self.tasks}
+        
         self.stats = {task: defaultdict(float) for task in self.tasks}
 
     def record(self, episode_return: float, episode_length: int, episode_task, env_id=None):
@@ -41,25 +43,26 @@ class StatRecorder:
         """
 
         if episode_task in self.tasks:
-            if self.calc_past_N:
+            if self.calc_past_n is not None:
                 self.episode_returns[episode_task].append(episode_return)
                 self.episode_lengths[episode_task].append(episode_length)
                 self.env_ids[episode_task].append(env_id)
 
-                self.stats[episode_task]['mean_r'] = np.mean(list(self.episode_returns[episode_task])[-self.calc_past_N:]) # I am not sure whether there is a more efficient way to slice to deque. I temperorily convert it to a list then slice it, which should cost O(n)
-                self.stats[episode_task]['var_r'] = np.var(list(self.episode_returns[episode_task])[-self.calc_past_N:])
-                self.stats[episode_task]['mean_l'] = np.mean(list(self.episode_lengths[episode_task])[-self.calc_past_N:])
-                self.stats[episode_task]['var_l'] = np.var(list(self.episode_lengths[episode_task])[-self.calc_past_N:])
+                self.stats[episode_task]['mean_r'] = np.mean(self.episode_returns[episode_task])
+                self.stats[episode_task]['var_r'] = np.var(self.episode_returns[episode_task])
+                self.stats[episode_task]['mean_l'] = np.mean(self.episode_lengths[episode_task])
+                self.stats[episode_task]['var_l'] = np.var(self.episode_lengths[episode_task])
             else:
                 # save the mean/variance of all the episodes
-                N_past = len(self.episode_returns[episode_task])
+                n_past = self.num_past_episodes[episode_task]
+                self.num_past_episodes[episode_task] += 1
                 
-                self.stats[episode_task]['mean_r'] = (self.stats[episode_task]['mean_r'] * N_past + episode_return) / (N_past + 1)
-                self.stats[episode_task]['mean_r_squared'] = (self.stats[episode_task]['mean_r_squared'] * N_past + episode_return ** 2) / (N_past + 1)
+                self.stats[episode_task]['mean_r'] = (self.stats[episode_task]['mean_r'] * n_past + episode_return) / (n_past + 1)
+                self.stats[episode_task]['mean_r_squared'] = (self.stats[episode_task]['mean_r_squared'] * n_past + episode_return ** 2) / (n_past + 1)
                 self.stats[episode_task]['var_r'] = self.stats[episode_task]['mean_r_squared'] - self.stats[episode_task]['mean_r'] ** 2
                 
-                self.stats[episode_task]['mean_l'] = (self.stats[episode_task]['mean_l'] * N_past + episode_length) / (N_past + 1)
-                self.stats[episode_task]['mean_l_squared'] = (self.stats[episode_task]['mean_l_squared'] * N_past + episode_length ** 2) / (N_past + 1)
+                self.stats[episode_task]['mean_l'] = (self.stats[episode_task]['mean_l'] * n_past + episode_length) / (n_past + 1)
+                self.stats[episode_task]['mean_l_squared'] = (self.stats[episode_task]['mean_l_squared'] * n_past + episode_length ** 2) / (n_past + 1)
                 self.stats[episode_task]['var_l'] = self.stats[episode_task]['mean_l_squared'] - self.stats[episode_task]['mean_l'] ** 2
         else:
             raise ValueError("Unknown task")
