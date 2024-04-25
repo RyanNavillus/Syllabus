91d7060f54fd8a0261544acf3b9f67207d0b153c
Ameen Ur Rehman <ameenreh2003@gmail.com>
2024-04-26 01:43:44 +0530

    added rollout class for task_sampler and fixed the GAE.

--- syllabus/curricula/plr/task_sampler.py
+++ syllabus/curricula/plr/task_sampler.py
@@ -4,7 +4,166 @@
 import gymnasium as gym
 import numpy as np
 import torch
+import warnings
 from typing import List
+
+from syllabus.core import Curriculum, UsageError, enumerate_axes
+
+
+class RolloutStorage(object):
+    def __init__(
+        self,
+        num_steps: int,
+        num_processes: int,
+        requires_value_buffers: bool,
+        observation_space: gym.Space,
+        action_space: gym.Space = None,
+        get_value=None,
+    ):
+        self.num_steps = num_steps
+        self.buffer_steps = num_steps * 2  # Hack to prevent overflow from lagging updates.
+        self.num_processes = num_processes
+        self._requires_value_buffers = requires_value_buffers
+        self._get_value = get_value
+        self.tasks = torch.zeros(self.buffer_steps, num_processes, 1, dtype=torch.int)
+        self.masks = torch.ones(self.buffer_steps + 1, num_processes, 1)
+        self.obs = [[[0] for _ in range(self.num_processes)]] * self.buffer_steps
+        self._fill = torch.zeros(self.buffer_steps, num_processes, 1)
+        self.env_steps = [0] * num_processes
+        self.should_update = False
+
+        if requires_value_buffers:
+            self.returns = torch.zeros(self.buffer_steps + 1, num_processes, 1)
+            self.rewards = torch.zeros(self.buffer_steps, num_processes, 1)
+            self.value_preds = torch.zeros(self.buffer_steps + 1, num_processes, 1)
+        else:
+            if action_space is None:
+                raise ValueError(
+                    "Action space must be provided to PLR for strategies 'policy_entropy', 'least_confidence', 'min_margin'"
+                )
+            self.action_log_dist = torch.zeros(self.buffer_steps, num_processes, action_space.n)
+
+        self.num_steps = num_steps
+        self.step = 0
+
+    def to(self, device):
+        self.masks = self.masks.to(device)
+        self.tasks = self.tasks.to(device)
+        self._fill = self._fill.to(device)
+        if self._requires_value_buffers:
+            self.rewards = self.rewards.to(device)
+            self.value_preds = self.value_preds.to(device)
+            self.returns = self.returns.to(device)
+        else:
+            self.action_log_dist = self.action_log_dist.to(device)
+
+    def insert(self, masks, action_log_dist=None, value_preds=None, rewards=None, tasks=None):
+        if self._requires_value_buffers:
+            assert (value_preds is not None and rewards is not None), "Selected strategy requires value_preds and rewards"
+            if len(rewards.shape) == 3:
+                rewards = rewards.squeeze(2)
+            self.value_preds[self.step].copy_(torch.as_tensor(value_preds))
+            self.rewards[self.step].copy_(torch.as_tensor(rewards)[:, None])
+            self.masks[self.step + 1].copy_(torch.as_tensor(masks)[:, None])
+        else:
+            self.action_log_dist[self.step].copy_(action_log_dist)
+        if tasks is not None:
+            assert isinstance(tasks[0], int), "Provided task must be an integer"
+            self.tasks[self.step].copy_(torch.as_tensor(tasks)[:, None])
+        self.step = (self.step + 1) % self.num_steps
+
+    def insert_at_index(self, env_index, mask=None, action_log_dist=None, obs=None, reward=None, task=None, steps=1):
+        if env_index >= self.num_processes:
+            warnings.warn(f"Env index {env_index} is greater than the number of processes {self.num_processes}. Using index {env_index % self.num_processes} instead.")
+            env_index = env_index % self.num_processes
+
+        step = self.env_steps[env_index]
+        end_step = step + steps
+        # Update buffer fill traacker, and check for common usage errors.
+        try:
+            if end_step > len(self._fill):
+                raise IndexError
+            self._fill[step:end_step, env_index] = 1
+        except IndexError as e:
+            if any(self._fill[:][env_index] == 0):
+                raise UsageError(f"Step {step} + {steps} = {end_step} is out of range for env index {env_index}. Your value for PLR's num_processes may be too high.") from e
+            else:
+                raise UsageError(f"Step {step} + {steps} = {end_step}  is out of range for env index {env_index}. Your value for PLR's num_processes may be too low.") from e
+
+        if mask is not None:
+            self.masks[step + 1:end_step + 1, env_index].copy_(torch.as_tensor(mask[:, None]))
+        if obs is not None:
+            for s in range(step, end_step):
+                self.obs[s][env_index] = obs[s - step]
+        if reward is not None:
+            self.rewards[step:end_step, env_index].copy_(torch.as_tensor(reward[:, None]))
+        if action_log_dist is not None:
+            self.action_log_dist[step:end_step, env_index].copy_(torch.as_tensor(action_log_dist[:, None]))
+        if task is not None:
+            try:
+                task = int(task)
+            except TypeError:
+                assert isinstance(task, int), f"Provided task must be an integer, got {task} with type {type(task)} instead."
+            self.tasks[step:end_step, env_index].copy_(torch.as_tensor(task))
+        else:
+            self.env_steps[env_index] += steps
+            # Hack for now, we call insert_at_index twice
+            while all(self._fill[self.step] == 1):
+                self.step = (self.step + 1) % self.buffer_steps
+                # Check if we have enough steps to compute a task sampler update
+                if self.step == self.num_steps + 1:
+                    self.should_update = True
+
+    def _get_values(self):
+        if self._get_value is None:
+            raise UsageError("Selected strategy requires value predictions. Please provide get_value function.")
+        for step in range(self.num_steps):
+            values = self._get_value(self.obs[step])
+            if len(values.shape) == 3:
+                warnings.warn(f"Value function returned a 3D tensor of shape {values.shape}. Attempting to squeeze last dimension.")
+                values = torch.squeeze(values, -1)
+            if len(values.shape) == 1:
+                warnings.warn(f"Value function returned a 1D tensor of shape {values.shape}. Attempting to unsqueeze last dimension.")
+                values = torch.unsqueeze(values, -1)
+            self.value_preds[step].copy_(values)
+
+    def after_update(self):
+        # After consuming the first num_steps of data, remove them and shift the remaining data in the buffer
+        self.tasks[0: self.num_steps].copy_(self.tasks[self.num_steps: self.buffer_steps])
+        self.masks[0: self.num_steps].copy_(self.masks[self.num_steps: self.buffer_steps])
+        self.obs[0: self.num_steps][:] = self.obs[self.num_steps: self.buffer_steps][:]
+
+        if self._requires_value_buffers:
+            self.returns[0: self.num_steps].copy_(self.returns[self.num_steps: self.buffer_steps])
+            self.rewards[0: self.num_steps].copy_(self.rewards[self.num_steps: self.buffer_steps])
+            self.value_preds[0: self.num_steps].copy_(self.value_preds[self.num_steps: self.buffer_steps])
+        else:
+            self.action_log_dist[0: self.num_steps].copy_(self.action_log_dist[self.num_steps: self.buffer_steps])
+
+        self._fill[0: self.num_steps].copy_(self._fill[self.num_steps: self.buffer_steps])
+        self._fill[self.num_steps: self.buffer_steps].copy_(0)
+
+        self.env_steps = [steps - self.num_steps for steps in self.env_steps]
+        self.should_update = False
+        self.step = self.step - self.num_steps
+
+    def compute_returns(self, rewards, values, masks, gamma, gae_lambda):
+        assert self._requires_value_buffers, "Selected strategy does not use compute_rewards."
+        self._get_values()
+        returns = np.zeros_like(rewards)
+        gae = 0
+        for step in reversed(range(self.num_steps)):
+            delta = (
+                    rewards[step]
+                    + gamma * values[step + 1] * masks[step + 1]
+                    - values[step]
+            )
+            gae = delta + gamma * gae_lambda * masks[step + 1] * gae
+            returns[step] = gae + values[step]
+
+
+def null(x):
+    return None
 
 
 class TaskSampler:
@@ -41,6 +200,8 @@ class TaskSampler:
         rho: float = 1.0,
         nu: float = 0.5,
         alpha: float = 1.0,
+        num_steps: int = 256,
+        num_processes: int = 1,
         gamma: float = 0.999,
         gae_lambda: float = 0.95,
         staleness_coef: float = 0.1,
@@ -49,6 +210,8 @@ class TaskSampler:
         robust_plr: bool = False,
         eval_envs = None,
         action_value_fn=None,
+        get_value=None,
+        observation_space = None
     ):
         self.action_space = action_space
         self.tasks = tasks
@@ -70,12 +233,25 @@ class TaskSampler:
         self.robust_plr = robust_plr
         self.eval_envs = eval_envs
         self.action_value_fn = action_value_fn
+        self.num_steps = num_steps
+        self.num_processes = num_processes
+        self._get_values = get_value
+        self.observation_space = observation_space
 
         self.unseen_task_weights = np.array([1.0] * self.num_tasks)
         self.task_scores = np.array([0.0] * self.num_tasks, dtype=float)
         self.partial_task_scores = np.zeros((num_actors, self.num_tasks), dtype=float)
         self.partial_task_steps = np.zeros((num_actors, self.num_tasks), dtype=np.int64)
         self.task_staleness = np.array([0.0] * self.num_tasks, dtype=float)
+
+        self._robust_rollouts = RolloutStorage(
+            self.num_steps,
+            self.num_processes,
+            self.requires_value_buffers,
+            self.observation_space,
+            action_space=action_space,
+            get_value=get_value if get_value is not None else null,
+        )
 
         self.next_task_index = 0  # Only used for sequential strategy
 
@@ -307,38 +483,6 @@ class TaskSampler:
 
         self._update_staleness(task_idx)
 
-        return task
-
-    def compute_returns(self, rewards, values, masks, gamma, gae_lambda):
-        assert self.requires_value_buffers, "Selected strategy does not use compute_rewards."
-
-        if isinstance(rewards, float):
-            rewards = [np.array(rewards)]
-
-        num_steps = len(rewards)
-        gae = 0
-        returns = np.zeros_like(rewards)
-        for step in reversed(range(num_steps)):
-            # Check if we are at the last step
-            if step == num_steps - 1:
-                delta = (rewards[step] - values[step])
-                gae = delta
-            else:
-                next_value = values[step + 1] if step + 1 < num_steps else 0
-                next_mask = masks[step + 1] if step + 1 < num_steps else 0
-                delta = (rewards[step]
-                         + gamma * next_value * next_mask
-                         - values[step]
-                         )
-                gae = delta + gamma * gae_lambda * next_mask * gae
-
-            gae_scal = gae[0] if isinstance(gae, np.ndarray) else gae
-            value_scal = values[step][0] if isinstance(values[step], np.ndarray) else values[step]
-
-            returns[step] = gae_scal + value_scal
-
-        return returns
-
     def evaluate_task(self, task, env, action_value_fn):
         if env is None:
             raise ValueError("Environment object is None. Please ensure it is properly initialized.")
@@ -352,11 +496,6 @@ class TaskSampler:
         while not done:
             action, value = action_value_fn(obs)
 
-            if isinstance(action, np.ndarray):
-                action = int(action[0])
-            else:
-                action = int(action)
-
             obs, rew, term, trunc, info = env.step(action)
 
             rewards.append(rew)
@@ -367,7 +506,7 @@ class TaskSampler:
             if term or trunc:
                 done = True
 
-        returns = self.compute_returns(rewards, values, masks, self.gamma, self.gae_lambda)
+        returns = self._robust_rollouts.compute_returns(rewards, values, masks, self.gamma, self.gae_lambda)
 
         return {
             "tasks": task,
@@ -403,7 +542,7 @@ class TaskSampler:
 
             # Otherwise, sample a new level
             if self.robust_plr:
-                self.update_with_episode_data(self._evaluate_unseen_level())
+                self._evaluate_unseen_level()
                 return self.sample(strategy=strategy)
             else:
                 return self._sample_unseen_level()
@@ -413,7 +552,7 @@ class TaskSampler:
                 return self._sample_replay_level()
             else:
                 if self.robust_plr:
-                    self.update_with_episode_data(self._evaluate_unseen_level())
+                    self._evaluate_unseen_level()
                     return self.sample(strategy=strategy)
                 else:
                     return self._sample_unseen_level()
