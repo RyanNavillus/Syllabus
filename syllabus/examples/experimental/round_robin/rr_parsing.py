ae74fa5762a36c88c763cdc79e73bf3d3dbe4c41
Ryan Pégoud <ryanpegoud@gmail.com>
2024-05-21 12:45:50 +0200

    improved round-robin parsing

--- syllabus/examples/experimental/round_robin/rr_parsing.py
+++ syllabus/examples/experimental/round_robin/rr_parsing.py
@@ -3,31 +3,15 @@ import json
 import os
 from json import JSONDecodeError
 
-import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-import plotly
-import plotly.graph_objects as go
+import seaborn as sns
 import wandb
 from tqdm import tqdm
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--directory",
-        required=True,
-        type=str,
-        help=(
-            "the round robin directory to parse,"
-            "should be in the following format:'DR_SP_DR_FSP'"
-        ),
-    )
-    parser.add_argument(
-        "--save",
-        type=bool,
-        default=False,
-        help="save the output json and return plots to wandb",
-    )
     parser.add_argument(
         "--logging-dir",
         type=str,
@@ -119,87 +103,145 @@ def merge_logs(dir: str, log_dir: str):
     return merged_logs
 
 
-def plot_returns(values: pd.Series, agent_name: str):
-    df = pd.DataFrame(values)
-    df.columns = ["values"]
-    df["avg"] = df["values"].apply(lambda x: np.array(x).mean())
-    df["std"] = df["values"].apply(lambda x: np.array(x).std())
+def plot_results(data: dict):
 
-    df["lower"] = df["avg"] - df["std"]
-    df["upper"] = df["avg"] + df["std"]
+    LABELSIZE = 36
+    TICKLABELSIZE = 24
 
-    fig = go.Figure()
+    fig, ax = plt.subplots(figsize=(7 * 2, 3.4 * 2))
+    color_palette = "colorblind"
+    color_palette = sns.color_palette("deep", n_colors=len(data.keys()))
+    colors = dict(zip(data.keys(), color_palette))
 
-    fig.add_trace(
-        go.Scatter(
-            x=df.index,
-            y=df["avg"],
-            mode="lines",
-            line=dict(color="blue"),
-            name="Average",
+    for alg in data.keys():
+        df = pd.DataFrame(data[alg])
+        df.columns = [int(col) for col in df.columns]
+        df = df.T.sort_index()
+        avg = df.mean(axis=1)
+        std = df.std(axis=1)
+        plt.plot(
+            df.index,
+            avg,
+            color=colors[alg],
+            marker="o",
+            linewidth=2,
+            label=alg,
         )
-    )
-
-    fig.add_trace(
-        go.Scatter(
-            x=np.array([df.index, df.index[::-1]]).flatten(),
-            y=np.array([df["upper"], df["lower"][::-1]]).flatten(),
-            fill="toself",
-            fillcolor="rgba(0, 0, 255, 0.2)",
-            line=dict(color="rgba(255, 255, 255, 0)"),
-            name="Error Band",
+        plt.fill_between(
+            df.index,
+            y1=avg + std,
+            y2=avg - std,
+            color=colors[alg],
+            alpha=0.2,
         )
-    )
 
-    fig.update_layout(
-        title=agent_name,
-        xaxis_title="Checkpoints",
-        yaxis_title="Returns",
-        showlegend=True,
+    plt.legend(
+        loc="lower center",
+        ncol=len(data.keys()),
+        bbox_to_anchor=(0.5, 1.0),
+        fontsize=30,
     )
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    ax.spines["left"].set_linewidth(2)
+    ax.spines["bottom"].set_linewidth(2)
+
+    ax.tick_params(length=0.1, width=0.1, labelsize=TICKLABELSIZE)
+    ax.spines["left"].set_position(("outward", 10))
+    ax.spines["bottom"].set_position(("outward", 10))
+    ax.set_xlabel("Steps", fontsize=LABELSIZE)
+    ax.set_ylabel("Mean Episodic Return", fontsize=LABELSIZE)
+    fig.tight_layout()
+    ax.grid(True, alpha=0.2)
+
     return fig
+
+
+def sort_and_trim_dicts(data: dict) -> dict:
+    min_length = min(len(sub_dict) for sub_dict in data.values())
+
+    def sort_and_trim(sub_dict, N):
+        sorted_keys = sorted(sub_dict.keys(), key=lambda x: int(x))[:N]
+        return {k: sub_dict[k] for k in sorted_keys}
+
+    trimmed_data = {
+        key: sort_and_trim(sub_dict, min_length) for key, sub_dict in data.items()
+    }
+
+    return trimmed_data
+
+
+def check_dir_name(dir: str, legal_names: list) -> bool:
+    split = dir.split("_")
+    return all([x in legal_names for x in split])
 
 
 if __name__ == "__main__":
     args = parse_args()
-    directory = f"{args.directory}/round-robin"
+    legal_names = ("DR", "PLR", "SP", "FSP", "PFSP")
+    legal_combinations = ("DR_SP", "DR_FSP", "DR_PFSP", "PLR_SP", "PLR_FSP", "PLR_PFSP")
+    valid_dirs = [dir for dir in os.listdir() if check_dir_name(dir, legal_names)]
+    print(f"valid directories: {valid_dirs}")
 
-    agent_path = directory.split("/")[0].split("_")
-    agent_1 = "_".join(agent_path[:2])
-    agent_2 = "_".join(agent_path[2:])
-    legal_names = ("DR_SP", "DR_FSP", "DR_PFSP", "PLR_SP", "PLR_FSP", "PLR_PFSP")
+    os.makedirs(args.logging_dir, exist_ok=True)
 
-    for agent in [agent_1, agent_2]:
-        assert (
-            agent in legal_names
-        ), f"Expected agent name in {legal_names} but got {agent}"
+    # parse the round-robin results, convert to json and save to logging dir
+    print("formatting round-robin results ...")
+    for dir in valid_dirs:
+        print(dir)
+        agent_path = dir.split("_")
+        agent_1 = "_".join(agent_path[:2])
+        agent_2 = "_".join(agent_path[2:])
 
-    logs = merge_logs(directory, args.logging_dir)
+        for agent in [agent_1, agent_2]:
+            assert (
+                agent in legal_combinations
+            ), f"Expected agent name in {legal_combinations} but got {agent}"
 
+        logs = merge_logs(dir, args.logging_dir)
+
+    # merge all the logs in logging dir and plot the result
+    print("merging results ...")
+    df = {}
+    logs = [file for file in os.listdir(args.logging_dir) if ".json" in file]
+    for log in logs:
+        print(log)
+        with open(os.path.join(args.logging_dir, log), "r") as f:
+            returns = json.load(f)
+
+            agent_1, agent_2 = list(returns.keys())
+            if agent_1 not in df.keys():
+                df[agent_1] = returns[agent_1]
+            if agent_2 not in df.keys():
+                df[agent_2] = returns[agent_2]
+            else:
+                for checkpoint in list(returns[agent_1].keys()):
+                    df[agent_1][checkpoint].extend(returns[agent_1][checkpoint])
+                for checkpoint in list(returns[agent_2].keys()):
+                    df[agent_2][checkpoint].extend(returns[agent_2][checkpoint])
+
+    with open(
+        os.path.join(args.logging_dir, "merged_rr_returns.json"),
+        "w",
+    ) as output_file:
+        json.dump(df, output_file, indent=4)
+
+    fig = plot_results(df)
+    fig.savefig(f"{args.logging_dir}/rr_returns.png", bbox_inches="tight")
+
+    print("logging ...")
     wandb.init(
         project=args.wandb_project_name,
         entity=args.wandb_entity,
         sync_tensorboard=True,
         config=vars(args),
-        name=f"round_robin__{agent_1}_{agent_2}",
-        monitor_gym=True,
+        name="round_robin_results",
         save_code=True,
         dir=args.logging_dir,
     )
+
     wandb.run.log_code(os.path.join(args.logging_dir))
-
-    if args.save:
-        returns_artifact = wandb.Artifact("returns", type="json")
-        returns_artifact.add_file(
-            os.path.join(args.logging_dir, f"{agent_1}_{agent_2}_returns.json")
-        )
-        wandb.log_artifact(returns_artifact)
-
-        df = pd.DataFrame(logs).sort_index()
-        df[f"{agent_1}"] = df[f"{agent_1}"].apply(lambda x: np.array(x))
-        df[f"{agent_2}"] = df[f"{agent_2}"].apply(lambda x: np.array(x))
-
-        fig = plot_returns(df[f"{agent_1}"], agent_1)
-        wandb.log({f"charts/{agent_1}_returns": wandb.Html(plotly.io.to_html(fig))})
-        fig = plot_returns(df[f"{agent_2}"], agent_2)
-        wandb.log({f"charts/{agent_2}_returns": wandb.Html(plotly.io.to_html(fig))})
+    returns_artifact = wandb.Artifact("returns", type="json")
+    returns_artifact.add_file(os.path.join(args.logging_dir, "merged_rr_returns.json"))
+    wandb.log_artifact(returns_artifact)
+    wandb.log({"charts/rr_returns": wandb.Image(fig)})
