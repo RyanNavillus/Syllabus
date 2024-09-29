7c09abd4c9ffa008cfd10643a78f7e0abe82dcf2
Ryan Sullivan <ryanpnavillus@gmail.com>
2024-09-29 18:39:34 -0400

    Document evaluator

--- syllabus/core/evaluator.py
+++ syllabus/core/evaluator.py
@@ -1,18 +1,44 @@
+from typing import Any, Callable, Dict, Optional, Sequence, Tuple, Union
 import warnings
+import numpy as np
 import torch
+from torch import Tensor
 from torch.distributions.categorical import Categorical
 
 from syllabus.core.utils import UsageError
 
 
-# TODO: Document this class
+Array = Union[np.ndarray, Tensor]
+LSTM_state = Tuple[Array, Array]
+
+
 class Evaluator:
-    def __init__(self, agent, device=None, preprocess_obs=None):
+    """An interface for evaluating a trained agent, used by several curricula."""
+    def __init__(self, agent: Any, device: Optional[torch.device] = None, preprocess_obs: Optional[Callable] = None):
+        """
+        Initialize the Evaluator.
+
+        Args:
+            agent (Any): The trained agent to be evaluated.
+            device (Optional[torch.device]): The device to run the evaluation on.
+            preprocess_obs (Optional[Any]): A function to preprocess observations.
+        """
         self.agent = agent
         self.device = device
         self.preprocess_obs = preprocess_obs
 
-    def get_value(self, state, lstm_state=None, done=None):
+    def get_value(self, state: Array, lstm_state: LSTM_state = None, done: Optional[Array] = None) -> Tuple[torch.Tensor, Dict[str, Any]]:
+        """
+        Get the value of a given environment state.
+
+        Args:
+            state (Array): The current environment state.
+            lstm_state (Optional[LSTM_state] ): The LSTM cell and hidden state.
+            done (Optional[Array]): The done flag.
+
+        Returns:
+            Tuple[torch.Tensor, Dict[str, Any]]: The value and additional information.
+        """
         state = self._prepare_state(state)
         if lstm_state is not None:
             lstm_state, done = self._prepare_lstm(lstm_state, done)
@@ -21,7 +47,18 @@ class Evaluator:
             value, lstm_state, extras = self._get_value(state, lstm_state=lstm_state, done=done)
         return value.to("cpu"), extras
 
-    def get_action(self, state, lstm_state=None, done=None):
+    def _get_action(self, state: Array, lstm_state: LSTM_state = None, done: Optional[Array] = None) -> Tuple[torch.Tensor, Dict[str, Any]]:
+        """
+        Sample an action from the policy for a given environment state.
+
+        Args:
+            state (Array): The current environment state.
+            lstm_state (Optional[LSTM_state]): The LSTM cell and hidden state.
+            done (Optional[Array]): The done flag.
+
+        Returns:
+            Tuple[torch.Tensor, Dict[str, Any]]: The action and additional information.
+        """
         state = self._prepare_state(state)
         if lstm_state is not None:
             lstm_state, done = self._prepare_lstm(lstm_state, done)
@@ -30,7 +67,18 @@ class Evaluator:
             action, lstm_state, extras = self._get_action(state, lstm_state=lstm_state, done=done)
         return action.to("cpu"), extras
 
-    def get_action_and_value(self, state, lstm_state=None, done=None):
+    def _get_action_and_value(self, state: Array, lstm_state: LSTM_state = None, done: Optional[Array] = None) -> Tuple[Tensor, Tensor, Dict[str, Any]]:
+        """
+        Get the action and value for a given environment state.
+
+        Args:
+            state (Array): The current environment state.
+            lstm_state (Optional[LSTM_state]): The LSTM cell and hidden state.
+            done (Optional[Array]): The done flag.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor, Dict[str, Any]]: The action, value, and additional information.
+        """
         state = self._prepare_state(state)
         if lstm_state is not None:
             lstm_state, done = self._prepare_lstm(lstm_state, done)
@@ -39,16 +87,60 @@ class Evaluator:
             action, value, extras = self._get_action_and_value(state, lstm_state=lstm_state, done=done)
         return action.to("cpu"), value.to("cpu"), extras
 
-    def _get_value(self, state, lstm_state=None, done=None):
+    def _get_value(self, state: Array, lstm_state: LSTM_state = None, done: Optional[Array] = None) -> Tuple[torch.Tensor, Dict[str, Any]]:
+        """
+        Abstract method to get the value of a given environment state.
+        Can be overridden to interface with different agent implementations.
+
+        Args:
+            state (Array): The current state.
+            lstm_state (Optional[LSTM_state]): The LSTM state.
+            done (Optional[Array]): The done flag.
+
+        Returns:
+            Tuple[torch.Tensor, Dict[str, Any]]: The value and additional information.
+        """
         raise NotImplementedError
 
-    def _get_action(self, state, lstm_state=None, done=None):
+    def _get_action(self, state: Array, lstm_state: LSTM_state = None, done: Optional[Array] = None) -> Tuple[torch.Tensor, Dict[str, Any]]:
+        """
+        Abstract method to get the action for a given environment state.
+        Can be overridden to interface with different agent implementations.
+
+        Args:
+            state (Array): The current state.
+            lstm_state (Optional[LSTM_state]): The LSTM state.
+            done (Optional[Array]): The done flag.
+
+        Returns:
+            Tuple[torch.Tensor, Dict[str, Any]]: The action and additional information.
+        """
         raise NotImplementedError
 
-    def _get_action_and_value(self, state, lstm_state=None, done=None):
+    def _get_action_and_value(self, state: Array, lstm_state: LSTM_state = None, done: Optional[Array] = None) -> Tuple[torch.Tensor, torch.Tensor, Dict[str, Any]]:
+        """
+        Abstract method to get the action and value for a given state.
+
+        Args:
+            state (Array): The current state.
+            lstm_state (Optional[LSTM_state]): The LSTM state.
+            done (Optional[Array]): The done flag.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor, Dict[str, Any]]: The action, value, and additional information.
+        """
         raise NotImplementedError
 
-    def _prepare_state(self, state):
+    def _prepare_state(self, state: Array) -> torch.Tensor:
+        """
+        Prepare the state for evaluation.
+
+        Args:
+            state (Array): The current state.
+
+        Returns:
+            torch.Tensor: The prepared state.
+        """
         if self.preprocess_obs is not None:
             state = self.preprocess_obs(state)
         state = torch.Tensor(state)
@@ -56,7 +148,17 @@ class Evaluator:
             state = state.to(self.device)
         return state
 
-    def _prepare_lstm(self, lstm_state, done):
+    def _prepare_lstm(self, lstm_state: LSTM_state, done: Array) -> Tuple[LSTM_state, torch.Tensor]:
+        """
+        Prepare the LSTM state and done flag for evaluation.
+
+        Args:
+            lstm_state (Tuple[Any, Any]): The LSTM state.
+            done (Any): The done flag.
+
+        Returns:
+            Tuple[Tuple[torch.Tensor, torch.Tensor], torch.Tensor]: The prepared LSTM state and done flag.
+        """
         lstm_state = (
             torch.Tensor(lstm_state[0]),
             torch.Tensor(lstm_state[1]),
